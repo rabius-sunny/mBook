cec5266e506eae0baba39318fd3624983a54daf9
Rabius Sunny <rabiussunnyc3x@gmail.com>
2021-05-19 04:17:44 +0600

    post section

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -2,13 +2,36 @@ import CreatePost from './CreatePost'
 import './feed.css'
 import Post from './Post'
 import Stories from './Stories'
+import pic from '../../images/pro.jpg'
+import ocean from '../../images/ocean.jpg'
+import ocean3 from '../../images/ocean3.jpg'
 
 const Feed = () => {
     return (
         <div className="feed">
             <Stories />
             <CreatePost />
-            <Post />
+            <Post
+                proPic={pic}
+                username="Rabius Sunny"
+                post="This is a post"
+                timestamp={new Date()}
+            />
+            <Post
+                proPic={pic}
+                username="Rabius Sunny"
+                post="This is a post"
+                image={ocean}
+                timestamp={new Date()}
+            />
+            
+            <Post
+                proPic={pic}
+                username="Rabius Sunny"
+                post="This is a post"
+                image={ocean3}
+                timestamp={new Date()}
+            />
         </div>
     )
 }
--- src/components/Feed/Post.jsx
+++ src/components/Feed/Post.jsx
@@ -1,4 +1,5 @@
 import { Avatar } from '@material-ui/core'
+import { ThumbUp, ChatBubbleOutline, NearMe, AccountCircle, ExpandMore } from '@material-ui/icons'
 import './feed.css'
 
 const Post = ({ proPic, image, username, timestamp, post }) => {
@@ -9,7 +10,31 @@ const Post = ({ proPic, image, username, timestamp, post }) => {
                     className="postAvatar" />
                 <div className="postInfo">
                     <h3>{username}</h3>
-                    <p>{new Date(timestamp?.toDate()).toUTCString()}</p>
+                    <p>{new Date(timestamp).toUTCString()}</p>
+                </div>
+            </div>
+            <div className="postBottom">
+                <p>{post}</p>
+            </div>
+            <div className="postImage">
+                <img src={image} alt="" />
+            </div>
+            <div className="postButtons">
+                <div className="postButton">
+                    <ThumbUp />
+                    <p>Like</p>
+                </div>
+                <div className="postButton">
+                    <ChatBubbleOutline />
+                    <p>Comment</p>
+                </div>
+                <div className="postButton">
+                    <NearMe />
+                    <p>Share</p>
+                </div>
+                <div className="postButton">
+                    <AccountCircle />
+                    <ExpandMore />
                 </div>
             </div>
         </div>
